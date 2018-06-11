6591d4ab95102e673fe6380fdadd1e2fdd0b7b79
thuclfc <thucnguyendac@sohagame.vn>
2018-06-11 18:42:25 +0700

    update

--- cheese/assets/js/page.js
+++ cheese/assets/js/page.js
@@ -9,3 +9,19 @@ function box_tab(tab,tab_content,tab_active,index_active) {
         $(tab_content).hide(); $(tab_content).eq(tab_index).show();
     });
 }
+
+function open_popup(btn,popup) {
+    $(btn).click(function (e) {
+        e.preventDefault();
+        $(popup).addClass('show');
+    });
+    $('.close_popup').click(function () {
+        $(popup).removeClass('show');
+    });
+}
+var modal = document.getElementsByClassName('modal');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
