3912fde21e086c3ddb6351b6d509a3cbdf5a4b50
thuclfc <thucnguyendac@sohagame.vn>
2018-06-13 18:35:22 +0700

    update

--- cheese/assets/js/page.js
+++ cheese/assets/js/page.js
@@ -1,12 +1,13 @@
 function box_tab(tab,tab_content,tab_active,index_active) {
-    $(tab_content).hide();
+    $(tab_content).addClass('hidden_tab');
     $(tab).eq(tab_active).addClass('active');
-    $(tab_content).eq(index_active).show();
+    $(tab_content).eq(index_active).addClass('show_tab');
     $(tab).click(function () {
         $(tab).removeClass('active');
         $(this).addClass('active');
         var tab_index=$(this).index();
-        $(tab_content).hide(); $(tab_content).eq(tab_index).show();
+        $(tab_content).addClass('hidden_tab').removeClass('show_tab');
+        $(tab_content).eq(tab_index).addClass('show_tab');
     });
 }
 
