a18e34d2de6784b1de39cf7854a9a17ae22a0756
thuclfc <thucnguyendac@sohagame.vn>
2018-08-01 17:10:02 +0700

    update

--- material_kit/assets/js/page_all.js
+++ material_kit/assets/js/page_all.js
@@ -59,3 +59,15 @@ $(document).mouseup(function(e){
         $('.effect_border,.select_b').removeClass('focus');
     }
 });
+
+function tabs(tab,tab_content,tab_active,index_active) {
+    $(tab_content).hide();
+    $(tab).eq(tab_active).addClass('active');
+    $(tab_content).eq(index_active).show();
+    $(tab).click(function () {
+        $(tab).removeClass('active');
+        $(this).addClass('active');
+        var tab_index=$(this).index();
+        $(tab_content).hide(); $(tab_content).eq(tab_index).show();
+    });
+}
